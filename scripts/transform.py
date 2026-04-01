20db4cac318139fbc9fa1a5bf94098ebd2eab8a2
Gilbert Kiprotich <gkiprotich74@gmail.com>
2026-04-01 06:47:36 +0000

    completed transform file

---
+++ scripts/transform.py
@@ -0,0 +1,20 @@
+import json
+import pandas as pd
+
+def transform_weather(input_path, output_path):
+    with open(input_path, 'r') as f:
+        data = json.load(f)
+
+    times = data['hourly']['time']
+    temps = data['hourly']['temperature_2m']
+
+    df = pd.DataFrame({
+        'time': times,
+        'temperature_2m': temps
+    })
+
+    df['time'] = pd.to_datetime(df['time'])
+
+    df.to_csv(output_path, index=False)
+
+    return output_path
